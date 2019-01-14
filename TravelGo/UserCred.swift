41859a34d0626a5afda28c17a79bc9e9c19eedc5
nikita <nikita1993chizh@gmail.com>
2019-01-14 23:55:06 +0300

    COreData

---
+++ TravelGo/UserCred.swift
@@ -0,0 +1,9 @@
+//
+//  UserCred.swift
+//  TravelGo
+//
+//  Created by Никита on 14/01/2019.
+//  Copyright © 2019 Никита. All rights reserved.
+//
+
+import Foundation
---
+++ TravelGo/UserCredentials.swift
@@ -0,0 +1,9 @@
+//
+//  UserCredentials.swift
+//  TravelGo
+//
+//  Created by Никита on 13/01/2019.
+//  Copyright © 2019 Никита. All rights reserved.
+//
+
+import Foundation
